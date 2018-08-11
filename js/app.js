58e6940df9e76c29a3150c20e6b712efef179b33
abranch7 <38540579+abranch7@users.noreply.github.com>
2018-08-11 08:53:55 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -36,3 +36,14 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+/*   
+*Start by building a very simple grid of cards. 
+Don't worry about styling, just get something clickable on the page.
+Figure out the HTML needed to represent a card. Remember, you have to represent two sides of the card. 
+Are you going to have two separate elements stacked on top of each other?
+Add the functionality to handle clicks.
+This should reveal the hidden side of each card.
+Work on the matching logic. How does your game "know" if a player guesses correctly or incorrectly?
+Work on the winning condition. How does your game “know” if a player has won?
+We recommend saving styling until the very end. Allow your game logic and functionality to dictate the styling.
+*/
